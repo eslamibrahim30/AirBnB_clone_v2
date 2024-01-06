e85513ce30909e1a779aa34fe7811dad3475d75f
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-06 08:04:48 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,16 +8,16 @@ sudo mkdir -p "/data/web_static/shared/"
 sudo mkdir -p "/data/web_static/releases/test/"
 sudo touch /data/web_static/releases/test/index.html
 sudo tee -a "/data/web_static/releases/test/index.html" << END
-<!DOCTYPE html>
 <html>
-    <head>
-	<meta charset="UTF-8">
-    </head>
-    <body>
-	<h1>First deployment</h1>
-    </body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>
 END
 sudo ln -s /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a \\n    location \/hbnb_static {\n        root /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
+sudo service nginx restart
+exit 0
