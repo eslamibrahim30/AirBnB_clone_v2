68061979657f8daed704041c11603bb7712436d6
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-06 13:55:03 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,7 @@ if command -v nginx &> /dev/null; then
 	sudo apt-get update
 	sudo apt-get install -y nginx
 fi
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo tee -a "/data/web_static/releases/test/index.html" << END
 <html>
@@ -18,7 +17,6 @@ sudo tee -a "/data/web_static/releases/test/index.html" << END
 END
 sudo ln -s /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-sudo chown -R ubuntu:ubuntu /data/*
 sudo sed -i '/server_name _;/a \\n    location \/hbnb_static {\n        root /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
