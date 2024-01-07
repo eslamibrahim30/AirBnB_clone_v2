01153fcf91ff0d9d3354dd3019fbfe524438162b
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-07 04:21:05 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-if command -v nginx &> /dev/null; then
-	sudo apt-get update
-	sudo apt-get install -y nginx
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo tee -a "/data/web_static/releases/test/index.html" << END
